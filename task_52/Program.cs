a55473cf2d338cb93780c5a26f55c0f92aa78fc2
AnnaKim <annakim@inbox.ru>
2023-01-30 11:16:09 +0300

    task_52 lines

---
+++ task_52/Program.cs
@@ -0,0 +1,52 @@
+﻿
+//task 52. Create 2-dimension array filled with integers 
+//find arithmetic average in each column
+//Example:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+// Arithmetic average of each column is: 4,6; 5,6; 3,6; 3.
+int [,] GetRandomArray (int rowLength, int colLength, int start, int end)
+{
+    int [,] array = new int [rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return array;
+}
+int [,] Array = GetRandomArray (3, 4, -10, 10);
+
+void Printarray (int [,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]}    ");
+        }
+        Console.WriteLine("    ");
+    }
+} 
+Printarray(Array);
+
+void GetAverage (int [,] Array)
+{  
+    double sum= 0;
+    int colNumber = 0;
+for (int j = 0; j < Array.GetLength(1); j++)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+     sum = sum + Array [i,j];
+    } 
+    double columnAverage = sum/Array.GetLength(0);
+    colNumber = colNumber + 1;
+    Console.WriteLine($"Arithmetic average of the column is {colNumber} : {columnAverage}"!);
+    sum = 0; 
+}
+}
+GetAverage(Array);
